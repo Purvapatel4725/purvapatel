df3a65254d717be0fc11f975211f69ff01649a9d
Purvapatel4725 <144143336+Purvapatel4725@users.noreply.github.com>
2024-08-18 09:37:02 -0400

    Minor change
    Co-Authored-By: Purva Patel <176457555+purva-patel@users.noreply.github.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: iPortfolio
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Updated: Jun 29 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 (function() {
   "use strict";
 
